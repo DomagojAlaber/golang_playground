a18426805aafe0e7fe74b66cf446dda68ff16d25
Domagoj <a.domagoj@hotmail.com>
2025-02-23 13:00:05 +0100

    Initial commit

---
+++ profit_calc/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64 = 25
+
+	fmt.Print("Enter your revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter your expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Enter the taxrate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := (revenue - expenses)
+	profit := ebt / (1 - taxRate/100)
+	ratio := ebt / profit
+
+	formattedRatio := fmt.Sprintf("Ratio: %.2f\n", ratio)
+
+	fmt.Print("Formatted Ratio: " + formattedRatio)
+
+	fmt.Printf("EBT %.2f\nProfit: %.2f\nRatio: %.2f\n", ebt, profit, ratio)
+}
